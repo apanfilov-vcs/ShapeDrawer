1d6f696b3d69bba06299874da7ad562d0e30ec13
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-12 10:34:47 -0700

    Fixed all issues with shapes and added event handlers, as well as optimal app configuration.

--- src/presentation-console/Program.cs
+++ src/presentation-console/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Diagnostics.Contracts;
+using application.Common;
+using domain;
+using domain.Shapes;
+
+var shapeApp = new ShapeBuilder
+    .Options()
+    .WithOutputHandler(Console.WriteLine)
+    .Build();
+
+var rectangle = shapeApp.CreateRectangle(20, 10);
+
+rectangle.Draw('@');
